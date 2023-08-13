d4cd377ac29aecc5fa828bc4f40be92e31fdf96a
Jesús Mata <andresmata.c21@gmail.com>
2023-08-13 00:55:20 -0600

    Add more testing

--- 04-giff-app/test/components/AddCategory.test.jsx
+++ 04-giff-app/test/components/AddCategory.test.jsx
@@ -19,7 +19,9 @@ describe('Pruebas en <AddCategory />', () => {
   test('debe de llamar onNewCategory si el input tiene un valor', () => {
     const inputValue = 'Elpepe'
 
-    render(<AddCategory onNewCategory={() => {}} />)
+    const onNewCategory = jest.fn()
+
+    render(<AddCategory onNewCategory={onNewCategory} />)
 
     const input = screen.getByRole('textbox')
     const form = screen.getByRole('form')
@@ -30,5 +32,20 @@ describe('Pruebas en <AddCategory />', () => {
     fireEvent.submit(form)
 
     expect(input.value).toBe('')
+    expect(onNewCategory).toHaveBeenCalled()
+    expect(onNewCategory).toHaveBeenCalledTimes(1)
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue)
+  })
+
+  test('no debe de llamar el onNewCategory si el input está vacío', () => {
+    const onNewCategory = jest.fn()
+
+    render(<AddCategory onNewCategory={onNewCategory} />)
+
+    const form = screen.getByRole('form')
+
+    fireEvent.submit(form)
+
+    expect(onNewCategory).toHaveBeenCalledTimes(0)
   })
 })
