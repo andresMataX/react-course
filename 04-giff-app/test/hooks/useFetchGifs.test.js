fc72ebcb1d64d52f0491bb4f2518cd4a282fbd23
Jesús Mata <andresmata.c21@gmail.com>
2023-08-13 14:54:53 -0600

    Add testing in custom hook

--- 04-giff-app/test/hooks/useFetchGifs.test.js
+++ 04-giff-app/test/hooks/useFetchGifs.test.js
@@ -1,15 +1,24 @@
-import { renderHook } from '@testing-library/react'
+import { renderHook, waitFor } from '@testing-library/react'
 import { useFetchGifs } from '../../src/hooks/useFetchGifs'
 
 describe('Pruebas en el hook useFetchGifs', () => {
   test('debe de regresar el estado inicial', () => {
     const { result } = renderHook(() => useFetchGifs('My chemical romance'))
 
-    console.log(result)
+    const { isLoading, images } = result.current
 
-    // const { isLoading, images } = result.current
+    expect(images.length).toBe(0)
+    expect(isLoading).toBeTruthy()
+  })
 
-    // expect(images.length).toBe(0)
-    // expect(isLoading).toBeTruthy()
+  test('debe de retornar un arreglo de imágenes e isLoading en false', async () => {
+    const { result } = renderHook(() => useFetchGifs('My chemical romance'))
+
+    await waitFor(() => expect(result.current.images.length).toBeGreaterThan(0))
+
+    const { isLoading, images } = result.current
+
+    expect(images.length).toBeGreaterThan(0)
+    expect(isLoading).toBeFalsy()
   })
 })
