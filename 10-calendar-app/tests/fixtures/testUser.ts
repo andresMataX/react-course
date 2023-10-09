db0e4a584ffe21246d5afffec613cb52c3a1685f
Jesús Mata <andresmata.c21@gmail.com>
2023-10-09 14:10:16 -0600

    Add testing in authSlice

--- 10-calendar-app/tests/fixtures/testUser.ts
+++ 10-calendar-app/tests/fixtures/testUser.ts
@@ -1,6 +1,4 @@
 export const testUserCredentials = {
-  email: 'test@gmail.com',
-  password: '123456',
   uid: 'ABC123',
   name: 'test',
 }
--- 10-calendar-app/tests/store/auth/authSlice.test.ts
+++ 10-calendar-app/tests/store/auth/authSlice.test.ts
@@ -1,7 +1,40 @@
-import { authSlice } from '../../../src/store/auth/authSlice'
-import { initialState } from '../../fixtures/authStates'
+import { authSlice, onLogin, onLogout } from '../../../src/store/auth/authSlice'
+import { authenticatedState, initialState } from '../../fixtures/authStates'
+import { testUserCredentials } from '../../fixtures/testUser'
 describe('Pruebas en authSlice', () => {
   test('debe de regresar el estado inicial', () => {
     expect(authSlice.getInitialState()).toEqual(initialState)
+  })
+
+  test('debe de realizar el login', () => {
+    const state = authSlice.reducer(initialState, onLogin(testUserCredentials))
+
+    expect(state).toEqual({
+      status: 'authenticated',
+      user: testUserCredentials,
+      errorMessage: undefined,
+    })
+  })
+
+  test('debe de realizar el logout', () => {
+    const state = authSlice.reducer(authenticatedState, onLogout())
+
+    expect(state).toEqual({
+      status: 'not-authenticated',
+      user: null,
+      errorMessage: undefined,
+    })
+  })
+
+  test('debe de realizar el logout con mensaje', () => {
+    const msg = 'Credenciales no válidas'
+
+    const state = authSlice.reducer(authenticatedState, onLogout(msg))
+
+    expect(state).toEqual({
+      status: 'not-authenticated',
+      user: null,
+      errorMessage: msg,
+    })
   })
 })
