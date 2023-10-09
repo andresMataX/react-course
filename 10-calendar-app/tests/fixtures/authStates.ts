cc979ddb3753a5d2244ab706ef4bcde8c8e3e9d0
Jesús Mata <andresmata.c21@gmail.com>
2023-10-09 13:54:38 -0600

    add authSlice tests

---
+++ 10-calendar-app/tests/fixtures/authStates.ts
@@ -0,0 +1,19 @@
+import { AuthState } from '../../src/store/auth/authSlice'
+
+export const initialState: AuthState = {
+  status: 'checking',
+  user: null,
+}
+
+export const authenticatedState: AuthState = {
+  status: 'authenticated',
+  user: {
+    name: 'pepe',
+    uid: '123456',
+  },
+}
+
+export const notAuthenticatedState: AuthState = {
+  status: 'not-authenticated',
+  user: null,
+}
---
+++ 10-calendar-app/tests/fixtures/testUser.ts
@@ -0,0 +1,6 @@
+export const testUserCredentials = {
+  email: 'test@gmail.com',
+  password: '123456',
+  uid: 'ABC123',
+  name: 'test',
+}
